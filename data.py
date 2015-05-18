66fd3943e12e8b00a0db1ddd1d9c8cd4eadf1cfc
gpsingh12 <gurpreet_1441@yahoo.com>
2015-05-18 01:40:37 +0000

    intial data

---
+++ data.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""List of names with scores"""
+
+intial_list = [
+    ('Jason', 70),
+    ('Mike', 65),
+    ('Nick', 82),
+    ('Anthony', 95),
+    ('Jack', 85),
+    ('Peter', 58),
+    ('Bill', 42),
+    ('Tim', 87),
+    ('Chris', 91),
+    ('Tom', 77),
+    ('Albert', 88),
+    ('John', 89),
+    ('Sunny', 73),
+    ('Mark', 81),
+    ('Dave',83),
+    ('David',97),
+    ('James', 93),
+    ('Justin', 68),
+    ('Joseph', 72),
+    ('Luke', 98),
+    ('Smith', 79),
+    ('William', 84),
+    ('Carlos', 86),
+    ('Pat', 90),
+    ('Jacob', 43),
+    ('Sean', 55),
+    ('Richard', 66),
+    ('Rick', 94),
+    ('Gary', 92),
+    ('Greg', 80),
+    ('Kevin', 74),
+    ('Jais', 32),
+    ('Will', 99),
+    ('Antonio', 22),
+    ('Roger', 19),
+    ('Sid', 96),
+    ('Bob',87),
+    ('Jose', 25),
+    ('Sam', 13),
+    ('Sharry', 17)
+]
+    
